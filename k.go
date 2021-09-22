fccfe480b3924834eb28affc08657687b626693e
AshotAikazyan <lakky992@gmail.com>
2021-09-22 16:38:50 +0700

    LAB1
    LABA

---
+++ k.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"log"
+)
+
+func FIO(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Айказян Ашот Ашотович 791")
+}
+func status(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "HTTP-статус 200")
+}
+func main() {
+	http.HandleFunc("/OK/", status) // Статус страницы
+	http.HandleFunc("/Info/", FIO) // Информация о студенте
+	err := http.ListenAndServe(":8080", nil) // устанавливаем порт веб-сервера
+	// Если хотите использовать https, то вместо ListenAndServe используйте ListenAndServeTLS
+	// err := http.ListenAndServeTLS(":8080", "cert.pem", "key.pem", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
